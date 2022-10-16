8073d6f23b6baf3e26174936d3c3d64ac83298c0
Daniel Bulhosa <daniel.woxi@gmail.com>
2022-10-16 14:26:08 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
     <>
       <h1 className='font-bold text-2xl text-violet-500'>Hello World</h1>
     
-      <button className='bg-cyan-400 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
+      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
         Enviar
       </button>
     </>
